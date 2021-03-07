3dbde5341c19a514448f03ba9ebf286331a35de5
adaskalopoulos <adaskalopoulos@localhost>
2021-03-07 13:29:04 -0500

    All the two letter combinations are in a vector for the poe poem. Next is finding the maximum.

--- englishcombinations.m
+++ englishcombinations.m
@@ -1,12 +1,16 @@
 %English 1800s
 url = "https://www.poetryfoundation.org/poems/48860/the-raven";
 code = webread(url);
-str = extractHTMLText(code);
-str_com = lower(str);
-str_com1 = str_com(37:6465);
-for i = 1:length(str_com1)-1
-    a = [str_com1(i),str_com1(i+1)]
+str_com = extractHTMLText(code);
+str_com1 = lower(str_com);
+str_com2 = str_com1(37:6465);
+str_poe1 = strings(5485,1);
+for i = 1 : 5485 - 1
+    str_poe1(i) = [str_com2(i),str_com2(i + 1)];
 end
-
-
-    
+com1 = str_poe1;
+str_poe2 = strings(945,1);
+for k = 5485:6429 - 1
+    str_poe2(k - 5484) = [str_com2(k),str_com2(k + 1)];  
+end
+com2 = str_poe2;
