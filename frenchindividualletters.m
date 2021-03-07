6108348fd671722eb0856129afe06096e7cbd7e7
ryanscarpa <79765617+ryanscarpa@users.noreply.github.com>
2021-03-07 12:09:08 -0500

    Add files via upload

--- frenchindividualletters.m
+++ frenchindividualletters.m
@@ -134,7 +134,7 @@ url = "https://fleursdumal.org/poem/200";
 code = webread(url);
 str3 = extractHTMLText(code);
 str3 = lower(str3);
-str4 = str3(74:809)
+str4 = str3(74:809);
 letter_values2 = zeros(1,40);
 for i = 1:length(str4)
     if (str4(i) == 'a') 
@@ -260,6 +260,268 @@ for i = 1:length(str4)
 end
 length(str3);
 
+%French 1600s
+url = "https://www.poemhunter.com/poem/la-belle-vieille/";
+code = webread(url);
+str5 = extractHTMLText(code);
+str5 = lower(str5);
+str6 = str5(754:4132);
+letter_values3 = zeros(1,40);
+for i = 1:length(str6)
+    if (str6(i) == 'a') 
+          letter_values3(1) = letter_values3(1)+1;
+      end    
+      if (str6(i) == 'â') 
+          letter_values3(2) = letter_values3(2)+1;
+      end
+      if (str6(i) == 'à') 
+          letter_values3(3) = letter_values3(3)+1;
+      end    
+      if (str6(i) == 'b') 
+          letter_values3(4) = letter_values3(4)+1;
+      end
+      if (str6(i) == 'c') 
+          letter_values3(5) = letter_values3(5)+1;
+      end    
+      if (str6(i) == 'ç') 
+          letter_values3(6) = letter_values3(6)+1;
+      end
+      if (str6(i) == 'd') 
+          letter_values3(7) = letter_values3(7)+1;
+      end    
+      if (str6(i) == 'e') 
+          letter_values3(8) = letter_values3(8)+1;
+      end
+      if (str6(i) == 'ê') 
+          letter_values3(9) = letter_values3(9)+1;
+      end    
+      if (str6(i) == 'é') 
+          letter_values3(10) = letter_values3(10)+1;
+      end
+      if (str6(i) == 'è') 
+          letter_values3(11) = letter_values3(11)+1;
+      end    
+      if (str6(i) == 'ë') 
+          letter_values3(12) = letter_values3(12)+1;
+      end
+      if (str6(i) == 'f') 
+          letter_values3(13) = letter_values3(13)+1;
+      end
+      if (str6(i) == 'g') 
+          letter_values3(14) = letter_values3(14)+1;
+      end    
+      if (str6(i) == 'h') 
+          letter_values3(15) = letter_values3(15)+1;
+      end
+      if (str6(i) == 'i') 
+          letter_values3(16) = letter_values3(16)+1;
+      end    
+      if (str6(i) == 'ï') 
+          letter_values3(17) = letter_values3(17)+1;
+      end
+      if (str6(i) == 'î') 
+          letter_values3(18) = letter_values3(18)+1;
+      end    
+      if (str6(i) == 'j') 
+          letter_values3(19) = letter_values3(19)+1;
+      end
+      if (str6(i) == 'k') 
+          letter_values3(20) = letter_values3(20)+1;
+      end    
+      if (str6(i) == 'l') 
+          letter_values3(21) = letter_values3(21)+1;
+      end
+      if (str6(i) == 'm') 
+          letter_values3(22) = letter_values3(22)+1;
+      end    
+      if (str6(i) == 'n') 
+          letter_values3(23) = letter_values3(23)+1;
+      end
+      if (str6(i) == 'o') 
+          letter_values3(24) = letter_values3(24)+1;
+      end    
+      if (str6(i) == 'ô') 
+          letter_values3(25) = letter_values3(25)+1;
+      end
+      if (str6(i) == 'œ') 
+          letter_values3(26) = letter_values3(26)+1;
+      end
+      if (str6(i) == 'p') 
+          letter_values3(27) = letter_values3(27)+1;
+      end
+      if (str6(i) == 'q') 
+          letter_values3(28) = letter_values3(28)+1;
+      end
+      if (str6(i) == 'r') 
+          letter_values3(29) = letter_values3(29)+1;
+      end
+      if (str6(i) == 's') 
+          letter_values3(30) = letter_values3(30)+1;
+      end
+      if (str6(i) == 't') 
+          letter_values3(31) = letter_values3(31)+1;
+      end
+      if (str6(i) == 'u') 
+          letter_values3(32) = letter_values3(32)+1;
+      end
+      if (str6(i) == 'ü') 
+          letter_values3(33) = letter_values3(33)+1;
+      end
+      if (str6(i) == 'û') 
+          letter_values3(34) = letter_values3(34)+1;
+      end
+      if (str6(i) == 'ù') 
+          letter_values3(35) = letter_values3(35)+1;
+      end
+      if (str6(i) == 'v') 
+          letter_values3(36) = letter_values3(36)+1;
+      end
+      if (str6(i) == 'w') 
+          letter_values3(37) = letter_values3(37)+1;
+      end
+      if (str6(i) == 'x') 
+          letter_values3(38) = letter_values3(38)+1;
+      end
+      if (str6(i) == 'y') 
+          letter_values3(39) = letter_values3(39)+1;
+      end
+      if (str6(i) == 'z') 
+          letter_values3(40) = letter_values3(40)+1;
+      end
+end
+length(str5);
+
+%French 1700s
+url = "https://poesie.webnet.fr/lesgrandsclassiques/poemes/jacques_delille/les_jardins";
+code = webread(url);
+str7 = extractHTMLText(code);
+str7 = lower(str7);
+str8 = str7(48:5585);
+letter_values4 = zeros(1,40);
+for i = 1:length(str8)
+    if (str8(i) == 'a') 
+          letter_values4(1) = letter_values4(1)+1;
+      end    
+      if (str8(i) == 'â') 
+          letter_values4(2) = letter_values4(2)+1;
+      end
+      if (str8(i) == 'à') 
+          letter_values4(3) = letter_values4(3)+1;
+      end    
+      if (str8(i) == 'b') 
+          letter_values4(4) = letter_values4(4)+1;
+      end
+      if (str8(i) == 'c') 
+          letter_values4(5) = letter_values4(5)+1;
+      end    
+      if (str8(i) == 'ç') 
+          letter_values4(6) = letter_values4(6)+1;
+      end
+      if (str8(i) == 'd') 
+          letter_values4(7) = letter_values4(7)+1;
+      end    
+      if (str8(i) == 'e') 
+          letter_values4(8) = letter_values4(8)+1;
+      end
+      if (str8(i) == 'ê') 
+          letter_values4(9) = letter_values4(9)+1;
+      end    
+      if (str8(i) == 'é') 
+          letter_values4(10) = letter_values4(10)+1;
+      end
+      if (str8(i) == 'è') 
+          letter_values4(11) = letter_values4(11)+1;
+      end    
+      if (str8(i) == 'ë') 
+          letter_values4(12) = letter_values4(12)+1;
+      end
+      if (str8(i) == 'f') 
+          letter_values4(13) = letter_values4(13)+1;
+      end
+      if (str8(i) == 'g') 
+          letter_values4(14) = letter_values4(14)+1;
+      end    
+      if (str8(i) == 'h') 
+          letter_values4(15) = letter_values4(15)+1;
+      end
+      if (str8(i) == 'i') 
+          letter_values4(16) = letter_values4(16)+1;
+      end    
+      if (str8(i) == 'ï') 
+          letter_values4(17) = letter_values4(17)+1;
+      end
+      if (str8(i) == 'î') 
+          letter_values4(18) = letter_values4(18)+1;
+      end    
+      if (str8(i) == 'j') 
+          letter_values4(19) = letter_values4(19)+1;
+      end
+      if (str8(i) == 'k') 
+          letter_values4(20) = letter_values4(20)+1;
+      end    
+      if (str8(i) == 'l') 
+          letter_values4(21) = letter_values4(21)+1;
+      end
+      if (str8(i) == 'm') 
+          letter_values4(22) = letter_values4(22)+1;
+      end    
+      if (str8(i) == 'n') 
+          letter_values4(23) = letter_values4(23)+1;
+      end
+      if (str8(i) == 'o') 
+          letter_values4(24) = letter_values4(24)+1;
+      end    
+      if (str8(i) == 'ô') 
+          letter_values4(25) = letter_values4(25)+1;
+      end
+      if (str8(i) == 'œ') 
+          letter_values4(26) = letter_values4(26)+1;
+      end
+      if (str8(i) == 'p') 
+          letter_values4(27) = letter_values4(27)+1;
+      end
+      if (str8(i) == 'q') 
+          letter_values4(28) = letter_values4(28)+1;
+      end
+      if (str8(i) == 'r') 
+          letter_values4(29) = letter_values4(29)+1;
+      end
+      if (str8(i) == 's') 
+          letter_values4(30) = letter_values4(30)+1;
+      end
+      if (str8(i) == 't') 
+          letter_values4(31) = letter_values4(31)+1;
+      end
+      if (str8(i) == 'u') 
+          letter_values4(32) = letter_values4(32)+1;
+      end
+      if (str8(i) == 'ü') 
+          letter_values4(33) = letter_values4(33)+1;
+      end
+      if (str8(i) == 'û') 
+          letter_values4(34) = letter_values4(34)+1;
+      end
+      if (str8(i) == 'ù') 
+          letter_values4(35) = letter_values4(35)+1;
+      end
+      if (str8(i) == 'v') 
+          letter_values4(36) = letter_values4(36)+1;
+      end
+      if (str8(i) == 'w') 
+          letter_values4(37) = letter_values4(37)+1;
+      end
+      if (str8(i) == 'x') 
+          letter_values4(38) = letter_values4(38)+1;
+      end
+      if (str8(i) == 'y') 
+          letter_values4(39) = letter_values4(39)+1;
+      end
+      if (str8(i) == 'z') 
+          letter_values4(40) = letter_values4(40)+1;
+      end
+end
+length(str7);
+
 h1 = figure;
 subplot(2,2,3)
 x = 1:40;
@@ -276,22 +538,27 @@ set(gca,'xtick',[1:40],'xticklabel',{'A','Â','À','B','C','Ç','D','E','Ê','É
 xlabel('Letters')
 ylabel('Frequency')
 title('Frequency of letters- L''Albatros (1900s)')
-% 
-% subplot(2,2,1)
-% x = 1:26;
-% bar(x, letter_values3/length(str6), 0.75, 'r');
-% set(gca,'xtick',[1:26],'xticklabel',{'A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
-% xlabel('Letters')
-% ylabel('Frequency')
-% title('Frequency of letters- The Phoenix and the Turtle (1600s)')
-% 
-% subplot(2,2,2)
-% x = 1:26;
-% bar(x, letter_values4/length(str8), 0.75, 'r');
-% set(gca,'xtick',[1:26],'xticklabel',{'A','B','C','D','E','F','G','H','I','J','K','L','N','M','O','P','Q','R','S','T','U','V','W','X','Y','Z'});
-% xlabel('Letters')
-% ylabel('Frequency')
-% title('Frequency of letters- On Imagination  (1700s)')
+
+subplot(2,2,1)
+x = 1:40;
+bar(x, letter_values3/length(str6), 0.75, 'r');
+set(gca,'xtick',[1:40],'xticklabel',{'A','Â','À','B','C','Ç','D','E','Ê','É','È','Ë','F','G','H','I','Ï','Î','J','K','L','M','N','O','Ô','Œ','P','Q','R','S','T','U','Ü','Û','Ù','V','W','X','Y','Z'});
+xlabel('Letters')
+ylabel('Frequency')
+title('Frequency of letters- La Belle Vieille (1600s)')
+
+subplot(2,2,2)
+x = 1:40;
+bar(x, letter_values4/length(str8), 0.75, 'r');
+set(gca,'xtick',[1:40],'xticklabel',{'A','Â','À','B','C','Ç','D','E','Ê','É','È','Ë','F','G','H','I','Ï','Î','J','K','L','M','N','O','Ô','Œ','P','Q','R','S','T','U','Ü','Û','Ù','V','W','X','Y','Z'});
+xlabel('Letters')
+ylabel('Frequency')
+title('Frequency of letters- Les jardins (1700s)')
+
+
+
+
+
 
 
 
