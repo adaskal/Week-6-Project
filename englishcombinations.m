01d6edd00f1bcc18f7a0d01e4a4fda8280ef4306
adaskalopoulos <adaskalopoulos@localhost>
2021-03-08 20:15:03 -0500

    The code now outputs a string of all the two letter combinations. There are no more combinations of letter and space or letter and puncuation. There is now a string that can be used to find the frequency of the most common combinations.

--- englishcombinations.m
+++ englishcombinations.m
@@ -4,12 +4,38 @@ code = webread(url);
 str_com = extractHTMLText(code);
 str_com1 = lower(str_com);
 str_com2 = str_com1(37:6465);
-str_poe1 = strings(6429,1);
-for i = 1 : 6428
+str_poe1 = strings(length(str_com2),1);
+for i = 1 : length(str_com2) - 1
     str_poe1(i) = [str_com2(i),str_com2(i + 1)];
 end
-a = transpose(str_poe1)
+a = transpose(str_poe1);
 my_str_of_1 = regexprep(a,'[^A-Za-z]+',' ');
 my_str1 = regexprep(my_str_of_1,'[^A-Za-z]+','');
-
+my_str1(my_str1 == "") = [];
+my_str1(my_str1 == "a") = [];
+my_str1(my_str1 == "b") = [];
+my_str1(my_str1 == "c") = [];
+my_str1(my_str1 == "d") = [];
+my_str1(my_str1 == "e") = [];
+my_str1(my_str1 == "f") = [];
+my_str1(my_str1 == "g") = [];
+my_str1(my_str1 == "h") = [];
+my_str1(my_str1 == "i") = [];
+my_str1(my_str1 == "j") = [];
+my_str1(my_str1 == "k") = [];
+my_str1(my_str1 == "l") = [];
+my_str1(my_str1 == "m") = [];
+my_str1(my_str1 == "n") = [];
+my_str1(my_str1 == "o") = [];
+my_str1(my_str1 == "p") = [];
+my_str1(my_str1 == "q") = [];
+my_str1(my_str1 == "r") = [];
+my_str1(my_str1 == "s") = [];
+my_str1(my_str1 == "t") = [];
+my_str1(my_str1 == "u") = [];
+my_str1(my_str1 == "v") = [];
+my_str1(my_str1 == "w") = [];
+my_str1(my_str1 == "x") = [];
+my_str1(my_str1 == "y") = [];
+my_str1(my_str1 == "z") = [];
 
