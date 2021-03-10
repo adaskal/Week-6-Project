20861e222d66debf448543ee4c0eaf916434294f
elenayjin <79773987+elenayjin@users.noreply.github.com>
2021-03-10 00:25:52 -0500

    outputs the 10 most frequent words and their frequencies

--- word_freq.m
+++ word_freq.m
@@ -17,7 +17,16 @@ v = values(unique_words);
 v = cell2mat(v);
 %maxk(v,5)
 
-[sorted_vals, sort_idx] = sort(v)
-sorted_keys = k(sort_idx)
+[sorted_vals, sort_idx] = sort(v);
+sorted_keys = k(sort_idx);
 
+%words = sorted_keys;
+%freq = sorted_vals;
 
+for ii = 1:10
+    words(ii) = sorted_keys(size(sorted_keys,2)-ii+1);
+    freq(ii) = sorted_vals(size(sorted_vals,2)-ii+1);
+end
+
+words
+freq
